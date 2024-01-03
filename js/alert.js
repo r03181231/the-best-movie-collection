e026304d8a3991df6a945b70399bab62b5d44d39
HaeRheeNam <4ran0318@gmail.com>
2024-01-03 22:18:50 +0900

    fix : 필수기능 완성 및 alert기능 파일 분리, 코드 재사용성 높여보기

---
+++ js/alert.js
@@ -0,0 +1,11 @@
+// alert.js - ex ) alert (`영화 Id : 123`)
+const movieIdAlert = (keyName, prop) => {
+  // 전체카드 중에서
+  for (let i = 0; i < prop.length; i++) {
+    //카드 하나 클릭 시,
+    prop[i].addEventListener("click", async (event) => {
+      let onetargetCardId = event.target.children[0].id;
+      alert(`${keyName} : ${onetargetCardId}`);
+    });
+  }
+};
