730a8586d83a0a5b545697ee1ba0875f10091d79
HaeRheeNam <4ran0318@gmail.com>
2024-01-05 00:28:05 +0900

    del : alert.js 파일 삭제, fix : 재검토 후 오류발생시키는 코드 수정, bugfix : card-container에서 null값 발생 시키는 것과 id가 undefind나오

--- js/alert.js
+++ js/alert.js
@@ -1,11 +1,15 @@
-// alert.js - ex ) alert (`영화 Id : 123`)
-const movieIdAlert = (keyName, prop) => {
-  // 전체카드 중에서
-  for (let i = 0; i < prop.length; i++) {
-    //카드 하나 클릭 시,
-    prop[i].addEventListener("click", async (event) => {
-      let onetargetCardId = event.target.children[0].id;
-      window.alert(`${keyName} : ${onetargetCardId}`);
-    });
-  }
-};
+// // import { searchBtnClick } from "../index.html";
+
+// // alert.js - ex ) alert (`영화 Id : 123`)
+// const movieIdAlert = (keyName, prop) => {
+//   // 전체카드 중에서
+//   for (let i = 0; i < prop?.length; i++) {
+//     // console.log(event);
+//     //카드 하나 클릭 시,
+//     prop[i].addEventListener("click", async (event) => {
+//       let onetargetCardId = event.target.children[0].id;
+//       console.log(onetargetCardId);
+//       window.alert(`${keyName} : ${onetargetCardId}`);
+//     });
+//   }
+// };
