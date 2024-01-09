ba182ac6c9dd1c065f2d6cc6361c17f3afd85b43
HaeRheeNam <4ran0318@gmail.com>
2024-01-09 17:25:47 +0900

    refactor : 변수 정리, 모듈 정리, 기능 정리, css 정리-주석 수정/ feat : 유효성 추가

--- js/common/search.js
+++ js/common/search.js
@@ -1,17 +1,34 @@
 import movieColl from "./movieColl.js";
 import { cards } from "./cards.js";
-import { $cards, $input } from "./values.js";
+import { $cards, $input, blank_pattern } from "./values.js";
 
 export const search = async () => {
   const movies = await movieColl();
   const inputValue = $input.value;
-  //키워드 포함한 영화 찾기
+  //키워드를 포함한 영화 찾기
   const equalCardData = movies.filter((movie) => {
     const upperValue = inputValue.toUpperCase();
-    return movie.title.toUpperCase().includes(upperValue);
+    const movieTitle = movie.title.toUpperCase();
+    const collect = movieTitle.includes(upperValue);
+
+    return collect;
   });
+  const regExp =
+    inputValue.replace(blank_pattern, "") === "" || equalCardData.length === 0;
+
+  // 검색input 유효성 - 공백만 있거나 일치하는 결과가 없을 때 해당 메세지 첨부하기.
+  if (regExp) {
+    // 이전 정보 비워줘.
+    $cards.innerHTML = "";
+    //유효성이 true면 아래 내역을 카드 내용 대신에 보여주기
+    return $cards.insertAdjacentHTML(
+      "beforeend",
+      `<div class="not-search">"<span>${inputValue}</span>"에 대한 검색 결과를 찾을 수 없습니다.</div>`
+    );
+  }
   // 이전 정보 비워주기
   $cards.innerHTML = "";
-  // 찾은 키워드에 대한 카드 정보를 채우기
+
+  //찾은 키워드에 대한 카드 정보를 채워주기
   cards(equalCardData);
 };
--- js/common/values.js
+++ js/common/values.js
@@ -1,3 +1,9 @@
+// values.js
 export const $searchBtn = document.querySelector("#searchBtn"); // 검색버튼
 export const $cards = document.querySelector("#cards-container"); // 카드 최상위 div
 export const $input = document.querySelector("#input");
+export const $goToPage = document.querySelector("#go-page");
+export const blank_pattern = /^\s+|\s+$/g; // 유효성 - 공백만 있을 경우
+export const goToPage = () => {
+  location.replace("https://tbmc.vercel.app/");
+};
